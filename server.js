349e71330486ee4aa3b44fdaec7145156d8faa96
pdji1602003 <pdji1602003@gmail.com>
2020-01-22 23:01:50 +0800

    added some comments

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+
+/* process 物件是一个全局变量，提供有关当前 Node.js 进程的信息并对其进行控制 */
+// 當process.用戶環境.NODE_ENV不等於 '生產環境'時，載入'dotenv'，並調用config()
+
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
 const express = require('express')
-const app = express()
+const app = express() // 創建一個基於express.js的application
 const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')//解析
 const path = require('path')
 
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
+
 
 app.set('view engine', 'ejs')//setting view engine
 app.set('views', path.join(__dirname,'/views')) //views to keep all the different views of our files for our server 
